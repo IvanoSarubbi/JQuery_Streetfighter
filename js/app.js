222121cd6c67b7792f8d8f3e587a9ee6e88ccfd2
IvanoSarubbi <beyonce@thinkful.com>
2015-09-21 23:50:33 -0400

    sound added

---
+++ js/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  });
+
+  function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
+  $('.ryu').mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  });
+
+  $('.ryu').mousedown(function() {
+  	playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+  .animate(
+    {'left': '1020px'},
+    500,
+    function() {
+      $(this).hide();
+      $(this).stop();
+      $(this).css('left', '520px');
+    }
+  );
+  });
+
+  $('.ryu').mouseup(function() {
+    $('.ryu-throwing').hide();
+  	$('.ryu-ready').show();
+  });
+
+ });
+
